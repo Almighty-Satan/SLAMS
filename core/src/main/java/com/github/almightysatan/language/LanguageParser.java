51b34d53609f0e606c9ea9e97c60e1670a5bfb73
Almighty-Satan <43804777+Almighty-Satan@users.noreply.github.com>
2023-04-03 11:15:49 +0200

    Change LanguageParser#load

--- core/src/main/java/com/github/almightysatan/language/LanguageParser.java
+++ core/src/main/java/com/github/almightysatan/language/LanguageParser.java
@@ -1,9 +1,12 @@
 package com.github.almightysatan.language;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.IOException;
 import java.util.Map;
+import java.util.Set;
 
 public interface LanguageParser { // TODO rename me
 
-    Map<String, Object> load() throws IOException;
+    @NotNull Map<String, Object> load(@NotNull Set<String> paths) throws IOException;
 }
