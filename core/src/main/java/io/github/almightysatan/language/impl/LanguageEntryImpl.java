b20dc7304285aa67edbfcf5817976875bb8a5a63
Almighty-Satan <43804777+Almighty-Satan@users.noreply.github.com>
2023-07-23 13:21:59 +0200

    Remove import

--- core/src/main/java/io/github/almightysatan/language/impl/LanguageEntryImpl.java
+++ core/src/main/java/io/github/almightysatan/language/impl/LanguageEntryImpl.java
@@ -1,6 +1,5 @@
 package io.github.almightysatan.language.impl;
 
-import com.github.almightysatan.language.*;
 import io.github.almightysatan.language.*;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
