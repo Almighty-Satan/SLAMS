8704be3dc5b2eddccf303f4d92eb7184ac5e92c7
Almighty-Satan <43804777+Almighty-Satan@users.noreply.github.com>
2023-09-08 15:56:50 +0200

    Exclude transitive dependencies

--- papi/build.gradle.kts
+++ papi/build.gradle.kts
@@ -9,6 +9,14 @@ repositories {
 
 dependencies {
     api(project(":core"))
-    implementation("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
+    implementation("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT") {
+        exclude("commons-lang")
+        exclude("com.googlecode.json-simple")
+        exclude("com.google.guava")
+        exclude("com.google.code.gson")
+        exclude("org.avaje")
+        exclude("org.yaml")
+        exclude("net.md-5")
+    }
     implementation("me.clip:placeholderapi:2.11.3")
 }
