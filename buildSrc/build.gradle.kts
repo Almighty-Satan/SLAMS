c0dfef12f4c6f15938ed8121df794fae103a34cc
Almighty-Satan <43804777+Almighty-Satan@users.noreply.github.com>
2024-03-10 09:33:17 +0100

    Fix build on newer Java versions

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,5 +1,20 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompilerOptions
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
+
 plugins {
     `kotlin-dsl`
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.named<KotlinCompilationTask<KotlinJvmCompilerOptions>>("compileKotlin").configure {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_1_8);
+    }
 }
 
 repositories {
