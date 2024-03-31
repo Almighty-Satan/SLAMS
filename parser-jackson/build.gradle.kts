5ff0af7d83f62c7e2dcbe6f5bd31a40d52f89ebf
Almighty-Satan <43804777+Almighty-Satan@users.noreply.github.com>
2024-03-31 12:05:52 +0200

    Bump Jackson to version 2.17.0

--- parser-jackson/build.gradle.kts
+++ parser-jackson/build.gradle.kts
@@ -4,6 +4,6 @@ plugins {
 
 dependencies {
     api(project(":core"))
-    api("com.fasterxml.jackson.core:jackson-databind:2.16.1")
+    api("com.fasterxml.jackson.core:jackson-databind:2.17.0")
     testImplementation(testFixtures(project(":core")))
 }
