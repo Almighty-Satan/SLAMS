8a63cfd75ceddeb21cde4dd00cc44adb2604bbec
Almighty-Satan <43804777+Almighty-Satan@users.noreply.github.com>
2024-11-18 19:16:37 +0100

    Bump jackson-databind version

--- parser-jackson/build.gradle.kts
+++ parser-jackson/build.gradle.kts
@@ -4,6 +4,6 @@ plugins {
 
 dependencies {
     api(project(":core"))
-    api("com.fasterxml.jackson.core:jackson-databind:2.18.0")
+    api("com.fasterxml.jackson.core:jackson-databind:2.18.1")
     testImplementation(testFixtures(project(":core")))
 }
