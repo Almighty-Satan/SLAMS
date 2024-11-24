cce4615021ae58ed9ad97fa18a844ae20244bec4
Almighty-Satan <43804777+Almighty-Satan@users.noreply.github.com>
2024-11-24 13:12:44 +0100

    Set spigot-api and placeholderapi to compileOnly

--- papi/build.gradle.kts
+++ papi/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 
 dependencies {
     api(project(":core"))
-    implementation("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT") {
+    compileOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT") {
         exclude("commons-lang")
         exclude("com.googlecode.json-simple")
         exclude("com.google.guava")
@@ -18,5 +18,5 @@ dependencies {
         exclude("org.yaml")
         exclude("net.md-5")
     }
-    implementation("me.clip:placeholderapi:2.11.6")
+    compileOnly("me.clip:placeholderapi:2.11.6")
 }
